0bbb9c1e88187784509b58cd5ead886d5765a756
shaunamooney <73603731+shaunamooney@users.noreply.github.com>
2024-04-03 15:01:37 +0100

    adding load_all() to demo script

--- demo/demo_script.R
+++ demo/demo_script.R
@@ -1,5 +1,5 @@
 # script to check functions for calculating country-level emus using the excel tool
-
+devtools::load_all()
 new_tool_filepath <- "~/Documents/GitHub/emu_output_analysis/data/SS to EMU Tool - Simple.xlsx"
 
 test_new_tools <- get_tools_info(new_tool_filepath)
