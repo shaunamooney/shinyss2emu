e8fa64ed8c3e015bb4bc80a9968e8bb4659afbe0
shaunamooney <73603731+shaunamooney@users.noreply.github.com>
2024-04-22 09:47:16 +0300

    fixing run_subnational bug

---
+++ demo/subnational_demo.R
@@ -0,0 +1,1 @@
+test_subnational <- run_subnational(subnational_tools_filepath = "~/Documents/GitHub/emu_output_analysis/data/test_data/DRC SS to EMU Subnational04192024/", input_shiny_type = "clients")
