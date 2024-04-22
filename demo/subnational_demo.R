0d04b2ef2b6013c7d78d736e02f50b9b6137e919
shaunamooney <73603731+shaunamooney@users.noreply.github.com>
2024-04-22 10:22:31 +0300

    updating demo

--- demo/subnational_demo.R
+++ demo/subnational_demo.R
@@ -1,1 +1,2 @@
+devtools::load_all()
 test_subnational <- run_subnational(subnational_tools_filepath = "~/Documents/GitHub/emu_output_analysis/data/test_data/DRC SS to EMU Subnational04192024/", input_shiny_type = "clients")
